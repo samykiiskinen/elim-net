b8a9228010b7b8c0b91119408b32fadd2692e5d4
samy <samykiiskinen@gmail.com>
2025-04-15 09:19:13 +0200

    Fix model types

---
+++ backend/Migrations/20250415071742_fix-model-types-second-try.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixmodeltypessecondtry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AidProjects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Verification",
+                table: "AidProjects",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Payment",
+                table: "AidProjects",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Income",
+                table: "AidProjects",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNo",
+                table: "AidProjects",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Verification",
+                table: "AidProjects",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Payment",
+                table: "AidProjects",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Income",
+                table: "AidProjects",
+                type: "numeric",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountNo",
+                table: "AidProjects",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AidProjects",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
